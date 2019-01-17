e99cbd44a0555ed6b29475209a9bb20da12c18ba
KhanhIceTea <khanhicetea@gmail.com>
2019-01-17 15:09:19 +0700

    add timestamp to commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,8 @@ git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
-git commit -m'action build' > /dev/null 2>&1 && \
+timestamp=$(date +%s%3N) && \
+git commit -m "Automated deployment to GitHub Pages on $timestamp" > /dev/null 2>&1 && \
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
