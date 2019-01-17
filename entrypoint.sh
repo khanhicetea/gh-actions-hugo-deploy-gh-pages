b8ef19e8a831e5ba3e963d381b56bedc0b76dd67
KhanhIceTea <khanhicetea@gmail.com>
2019-01-17 23:18:06 +0700

    git checkout --orphan

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,15 @@ cd dist
 remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages" && \
 git init && \
+git remote add deploy $remote_repo && \
+git checkout $remote_branch || git checkout --orphan $remote_branch && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
 timestamp=$(date +%s%3N) && \
 git commit -m "Automated deployment to GitHub Pages on $timestamp" > /dev/null 2>&1 && \
-git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
+git push deploy $remote_branch --force > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
 echo '=================== Done  ==================='
